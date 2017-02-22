9671a80ffb8939b08399c49f5badef3e947d9ddc
yashutanu <yashutanu1@gmail.com>
2017-02-22 18:38:20 +0530

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -21,25 +21,62 @@ app = Flask(__name__)
 def Welcome():
     return app.send_static_file('index.html')
 
-@app.route('/myapp')
-def WelcomeToMyapp():
-    return 'Welcome again to my app running on Bluemix!'
-
-@app.route('/api/people')
-def GetPeople():
-    list = [
-        {'name': 'John', 'age': 28},
-        {'name': 'Bill', 'val': 26}
-    ]
-    return jsonify(results=list)
-
-@app.route('/api/people/<name>')
-def SayHello(name):
-    message = {
-        'message': 'Hello ' + name
+book_info = [
+    {
+        'b_id': 1,
+        'category':1,
+        'name': 'spindle',
+        'price': 450, 
+        'quantity':10
+    },
+    {
+        'b_id': 2,
+        'category':1,
+        'name':'Lost Girl',
+        'price': 350, 
+        'quantity':20
+    },
+    {
+        'b_id': 3,
+        'category':1,
+        'name':  'Black Moon',
+        'price': 400, 
+        'quantity':30
+    },
+    {
+        'b_id': 4,
+        'category':2,
+        'name':  'my life',
+        'price': 300, 
+        'quantity':10
+    },
+    {
+        'b_id': 5,
+        'category':2,
+        'name':  'Victoria the queen',
+        'price': 600, 
+        'quantity':10
+    },
+    {
+        'b_id': 6,
+        'category':2,
+        'name': 'philosopher stone',
+        'price': 390, 
+        'quantity':2
     }
-    return jsonify(results=message)
+    
+]
 
+@app.route('/bookkart/book_info', methods=['GET'])
+def get_bookinfo():
+    return jsonify({'book_details': book_info})
+
+@app.route('/bookkart/book_info/<int:category>', methods=['GET'])
+def get_bookinfo_category(category):
+    books = [i for i in book_info if i['category'] == category]
+    if len(books) == 0:
+        abort(404)
+    return jsonify({'books': books})
 port = os.getenv('PORT', '5000')
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=int(port))
